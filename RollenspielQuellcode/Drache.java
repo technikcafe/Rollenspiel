048a69f5d299479172f5d688a0af4d96a0efd700
JonathanStrauss08 <jonathan.strauss@amplonius.de>
2025-12-11 09:20:08 +0100

    Comment der Methoden, Rechtschreibfehler berichtigung

--- RollenspielQuellcode/Drache.java
+++ RollenspielQuellcode/Drache.java
@@ -12,7 +12,7 @@ public class Drache{
 
     public void blocken(int pAngriff){
         if(zufallszahl(0,100)>80){
-            System.out.println("Der Drach weicht dem Angriff aus!");        
+            System.out.println("Der Drache weicht dem Angriff aus!");        
             return ;
         }else{
 
@@ -25,16 +25,22 @@ public class Drache{
             }
         }
     }
-
+    /**
+     * Die Methode angriff greift mit angriffschaden an, und zu 10% mit zehnfachen schaden an
+     */
     public int  angriff(){
         if(zufallszahl(0,100)>90){
             System.out.println("Der Drache speit Feuer und greift dich mit "+angriffschaden*10+" an");        
             return angriffschaden*10;
         }
-        System.out.println("Der Drache greift mit"+angriffschaden+" an");        
+        else {System.out.println("Der Drache greift mit"+angriffschaden+" an");        
         return angriffschaden;
+        }
     }
-
+    /**
+     * Die Methode gibt eine Zufallszahl zwischen einer Minimum und einem Maximum an und rundet sie auf eine Ganze zahl(int)
+     * @param: int min - niedrige Begrenzung zufallszahl   int max - obere Begrenzung zufallszahl
+     */
     private int zufallszahl(int min, int max){
         return (int) (Math.random()*(max-min)+min);
     }
